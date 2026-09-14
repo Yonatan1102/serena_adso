a3aecdabeea48feef72f511a055520be247a7c65
YONATAN CAMILO ACUÑA GONZALEZ <gonzalescamilo1102@gmail.com>
2026-09-14 20:03:29 +0000

    proxy en vite.config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,6 +12,14 @@ export default defineConfig(() => {
       },
     },
     server: {
+        proxy: {
+          '/api': {
+            target: 'http://localhost:5000', // Puerto de tu API Gateway
+            changeOrigin: true,
+            secure: false,
+          },
+        },
+
       // HMR is disabled in AI Studio via DISABLE_HMR env var.
       // Do not modifyâfile watching is disabled to prevent flickering during agent edits.
       hmr: process.env.DISABLE_HMR !== 'true',
